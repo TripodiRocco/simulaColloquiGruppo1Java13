9b06a8b9bf8f2c5ccc1634a55740ee84f9cffc51
Rocco <rocco.tripodi1994@gmail.com>
2023-09-04 17:37:25 +0200

    Perfezionata (ancora in corso) documentazione API

--- src/main/java/com/example/chatbotColloquio/service/ChatGptService.java
+++ src/main/java/com/example/chatbotColloquio/service/ChatGptService.java
@@ -20,7 +20,7 @@ import java.net.URL;
 public class ChatGptService implements GptService{
     // private static final String GPT_API_URL = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions";
     private static final String GPT_API_URL = "https://api.openai.com/v1/chat/completions";
-    private String apiKey = "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"; // chiave API
+    private String apiKey = "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"; // chiave API
     private String model = "gpt-3.5-turbo";
 
     @Autowired
